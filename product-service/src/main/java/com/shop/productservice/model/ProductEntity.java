c6a3eb47d194879da3287344156fec64e88f24f6
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-08 11:47:55 +0300

    create Product entity

---
+++ product-service/src/main/java/com/shop/productservice/model/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.shop.productservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+}
