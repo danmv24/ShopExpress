676ebc15a9bd50cad270cb54190033dd277e3edf
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-11 23:47:41 +0300

    entity to view

--- product-service/src/main/java/com/shop/productservice/mapper/ProductMapper.java
+++ product-service/src/main/java/com/shop/productservice/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.shop.productservice.mapper;
 import com.shop.productservice.form.ProductForm;
 import com.shop.productservice.model.CategoryEntity;
 import com.shop.productservice.model.ProductEntity;
+import com.shop.productservice.view.ProductView;
 
 public class ProductMapper {
 
@@ -12,6 +13,14 @@ public class ProductMapper {
                 .category(category)
                 .description(productForm.getDescription())
                 .price(productForm.getPrice())
+                .build();
+    }
+
+    public static ProductView toView(ProductEntity product) {
+        return ProductView.builder()
+                .productName(product.getProductName())
+                .description(product.getDescription())
+                .price(product.getPrice())
                 .build();
     }
 
