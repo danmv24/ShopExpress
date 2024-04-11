5ffc8e0dfacf3b871b566a460024a3f57c9e9729
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-11 23:46:18 +0300

    create product repository

---
+++ product-service/src/main/java/com/shop/productservice/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.shop.productservice.repository;
+
+import com.shop.productservice.model.ProductEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findAll();
+
+}
