49f410c153d687f400694161787f35e8ec3fe634
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-11 19:43:22 +0300

    query to create a products table

---
+++ product-service/src/main/resources/db/changelog/changes/create_table_products.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "Products" (
+    id BIGSERIAL PRIMARY KEY,
+    product_name VARCHAR(200) NOT NULL,
+    category_id BIGINT NOT NULL,
+    weight DECIMAL(10, 3),
+    description TEXT,
+    price DECIMAL(10, 2),
+    FOREIGN KEY (category_id) REFERENCES "Categories" (id)
+);
