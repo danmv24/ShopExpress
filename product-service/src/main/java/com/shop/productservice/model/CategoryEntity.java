09e2601bde1285da3f6d7aad204747e273a0f426
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-11 22:59:16 +0300

    add new annotations

--- product-service/src/main/java/com/shop/productservice/model/CategoryEntity.java
+++ product-service/src/main/java/com/shop/productservice/model/CategoryEntity.java
@@ -1,8 +1,16 @@
 package com.shop.productservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "Categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryEntity {
 
     @Id
