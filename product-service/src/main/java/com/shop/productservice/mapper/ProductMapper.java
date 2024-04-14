2ef8eee6ae70171efd12ad240ff5b1ab178be116
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-14 17:45:20 +0300

    add new field

--- product-service/src/main/java/com/shop/productservice/mapper/ProductMapper.java
+++ product-service/src/main/java/com/shop/productservice/mapper/ProductMapper.java
@@ -3,14 +3,17 @@ package com.shop.productservice.mapper;
 import com.shop.productservice.form.ProductForm;
 import com.shop.productservice.model.CategoryEntity;
 import com.shop.productservice.model.ProductEntity;
+import com.shop.productservice.model.SubcategoryEntity;
 import com.shop.productservice.view.ProductView;
 
 public class ProductMapper {
 
-    public static ProductEntity toEntity(ProductForm productForm, CategoryEntity category) {
+    public static ProductEntity toEntity(ProductForm productForm, CategoryEntity category, SubcategoryEntity subcategory) {
         return ProductEntity.builder()
                 .productName(productForm.getProductName())
                 .category(category)
+                .subcategory(subcategory)
+                .weight(productForm.getWeight())
                 .description(productForm.getDescription())
                 .price(productForm.getPrice())
                 .build();
@@ -19,6 +22,9 @@ public class ProductMapper {
     public static ProductView toView(ProductEntity product) {
         return ProductView.builder()
                 .productName(product.getProductName())
+                .categoryName(product.getCategory().getCategoryName())
+                .subcategoryName(product.getSubcategory().getSubcategoryName())
+                .weight(product.getWeight())
                 .description(product.getDescription())
                 .price(product.getPrice())
                 .build();
