52a476e33fa7e832f2685856257a387763b74258
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-14 17:46:47 +0300

    categories

---
+++ product-service/src/main/resources/db/changelog/changes/insert_data_into_the_categories_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (category_name) VALUES
+                                            ('Фрукты'),
+                                            ('Овощи'),
+                                            ('Зелень'),
+                                            ('Молочные продукты'),
+                                            ('Мясо');
