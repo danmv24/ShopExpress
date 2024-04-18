13a8e9d1b261237a6c8d0dd15dee039dac072c51
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-18 23:31:39 +0300

    An order service has been created to process orders

---
+++ order-service/src/main/java/com/orderservice/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.orderservice.service;
+
+public interface OrderService {
+
+
+
+}
---
+++ order-service/src/main/java/com/orderservice/service/impl/DefaultOrderService.java
@@ -0,0 +1,8 @@
+package com.orderservice.service.impl;
+
+import com.orderservice.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultOrderService implements OrderService {
+}
