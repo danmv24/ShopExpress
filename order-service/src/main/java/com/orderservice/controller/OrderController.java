c379c6a91f1ec38d37c8c626c600b73429839269
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-22 15:43:44 +0300

    add order service

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -1,15 +1,21 @@
 package com.orderservice.controller;
 
 import com.orderservice.form.OrderForm;
+import com.orderservice.service.OrderService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/order")
 public class OrderController {
 
+    private final OrderService orderService;
+
     public String createOrder(@RequestBody OrderForm orderForm) {
+        orderService.createOrder(orderForm);
         return "Заказ обрабатывается";
     }
 
