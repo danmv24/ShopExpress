606cc77dba95e7dba89eabddf2d4d3e9349c6487
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-24 19:22:34 +0300

    create order

--- order-service/src/main/java/com/orderservice/service/impl/DefaultOrderService.java
+++ order-service/src/main/java/com/orderservice/service/impl/DefaultOrderService.java
@@ -1,22 +1,60 @@
 package com.orderservice.service.impl;
 
+import com.orderservice.entity.OrderDetailEntity;
 import com.orderservice.entity.OrderEntity;
 import com.orderservice.form.OrderForm;
 import com.orderservice.mapper.OrderMapper;
+import com.orderservice.repository.OrderDetailRepository;
+import com.orderservice.repository.OrderRepository;
 import com.orderservice.service.OrderService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
+@Transactional
+@AllArgsConstructor
 public class DefaultOrderService implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private final OrderDetailRepository orderDetailRepository;
+
+    /**
+     * На будущее
+     * {
+     *   "items": [
+     *     {
+     *       "productName": "Яблоки Гренни Смит",
+     *       "quantity": 2,
+     *       "price": 150
+     *     },
+     *     {
+     *       "productName": "Яблоки Голден",
+     *       "quantity": 1,
+     *       "price": 1000
+     *     }
+     *   ],
+     *   "payment_method": "Кредитная карта"
+     * }
+     * */
 
     @Override
     public void createOrder(OrderForm orderForm) {
-        List<OrderEntity> orderEntities = orderForm.getOrderItems().stream()
-                .map(OrderMapper::toOrderEntity)
+        BigDecimal totalPrice = orderForm.getOrderItems().stream()
+                .map(orderItem -> orderItem.getPrice().multiply(BigDecimal.valueOf(orderItem.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        OrderEntity orderEntity = OrderMapper.toOrderEntity(totalPrice, DefaultDateTimeFormatterService.formatDateTime());
+
+        List<OrderDetailEntity> orderDetailEntities = orderForm.getOrderItems().stream()
+                .map(orderItem -> OrderMapper.toOrderDetailEntity(orderEntity, orderItem))
                 .toList();
 
+        orderDetailRepository.saveAll(orderDetailEntities);
     }
 
 }
