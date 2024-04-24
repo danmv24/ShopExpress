84c8a1b637407cea05e93b6cee675b0e10ed9225
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-24 19:18:24 +0300

    changes

--- order-service/src/main/java/com/orderservice/service/impl/DefaultDateTimeFormatterService.java
+++ order-service/src/main/java/com/orderservice/service/impl/DefaultDateTimeFormatterService.java
@@ -4,24 +4,18 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.ResolverStyle;
 
 @Service
 public class DefaultDateTimeFormatterService {
 
-    public static String formatDateTime() {
-        // Создаем форматировщик даты и времени
-        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-                .appendPattern("dd-MM-yyyy HH:mm:ss")
-                .toFormatter()
-                .withResolverStyle(ResolverStyle.STRICT);
-
-        // Получаем текущую дату и время
+    public static LocalDateTime formatDateTime() {
         LocalDateTime currentDateTime = LocalDateTime.now();
 
-        // Преобразуем дату и время в строку в соответствии с заданным форматом
-        return currentDateTime.format(formatter);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        return LocalDateTime.parse(formattedDateTime, formatter);
     }
 
 }
