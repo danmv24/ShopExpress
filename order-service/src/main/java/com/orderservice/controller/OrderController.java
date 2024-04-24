7a6463786e9b709a696958a4974c5e41780008ed
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-24 19:19:16 +0300

    add annotation PostMapping

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.orderservice.controller;
 import com.orderservice.form.OrderForm;
 import com.orderservice.service.OrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @PostMapping
     public String createOrder(@RequestBody OrderForm orderForm) {
         orderService.createOrder(orderForm);
         return "Заказ обрабатывается";
