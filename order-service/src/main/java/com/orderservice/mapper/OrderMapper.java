a9045fddd7c3eb1bafd00865afcf3b327477199a
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-24 19:19:36 +0300

    toOrderDetailEntity

--- order-service/src/main/java/com/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/orderservice/mapper/OrderMapper.java
@@ -1,16 +1,27 @@
 package com.orderservice.mapper;
 
+import com.orderservice.entity.OrderDetailEntity;
 import com.orderservice.entity.OrderEntity;
 import com.orderservice.form.OrderItem;
-import com.orderservice.service.impl.DefaultDateTimeFormatterService;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class OrderMapper {
 
-    public static OrderEntity toOrderEntity(OrderItem orderItem) {
+    public static OrderEntity toOrderEntity(BigDecimal totalPrice, LocalDateTime date) {
         return OrderEntity.builder()
-                .orderDate(LocalDateTime.parse(DefaultDateTimeFormatterService.formatDateTime()))
+                .totalPrice(totalPrice)
+                .orderDate(date)
+                .build();
+    }
+
+    public static OrderDetailEntity toOrderDetailEntity(OrderEntity order, OrderItem orderItem) {
+        return OrderDetailEntity.builder()
+                .order(order)
+                .productId(1L)
+                .quantity(orderItem.getQuantity())
+                .unitPrice(orderItem.getPrice())
                 .build();
     }
 
