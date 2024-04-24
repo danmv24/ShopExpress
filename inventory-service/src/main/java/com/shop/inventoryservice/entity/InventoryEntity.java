692c3b80beeecafb2a968f7672835225da8a8949
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-24 15:40:21 +0300

    inventory entity

---
+++ inventory-service/src/main/java/com/shop/inventoryservice/entity/InventoryEntity.java
@@ -0,0 +1,23 @@
+package com.shop.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+}
