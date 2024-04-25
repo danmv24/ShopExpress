02b73d5946774758fc9d7c04f16ea96f21cf26ca
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-25 18:41:13 +0300

    created method findIdByProductName

--- product-service/src/main/java/com/shop/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/shop/productservice/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.shop.productservice.repository;
 
 import com.shop.productservice.model.ProductEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,8 @@ import java.util.List;
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
 
     List<ProductEntity> findAll();
+
+    @Query(value = "SELECT id FROM products WHERE productName = :productName")
+    Long findIdByProductName(String productName);
 
 }
