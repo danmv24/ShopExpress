a1fbcd2b07c26d40ba8e0d9ea8fa1365adf49718
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-25 23:44:19 +0300

    the inventory client was created to send requests to the inventory-service

---
+++ order-service/src/main/java/com/orderservice/client/InventoryClient.java
@@ -0,0 +1,16 @@
+package com.orderservice.client;
+
+import com.orderservice.response.InventoryResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "inventory", url = "${inventory.url}")
+public interface InventoryClient {
+
+    @GetMapping(value = "/api/inventory")
+    List<InventoryResponse> getProducts(@RequestParam(name = "productsId") List<Long> productsId);
+
+}
