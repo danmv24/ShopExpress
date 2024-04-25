a6f19ecb80dae1c838fece85dc5c0d8610c0b3cd
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-25 23:45:22 +0300

    a point has been created to get product information from the inventory-service

--- inventory-service/src/main/java/com/shop/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.shop.inventoryservice.controller;
 
+import com.shop.inventoryservice.response.InventoryResponse;
 import com.shop.inventoryservice.service.InventoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -12,10 +15,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{productId}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("productId") Long productId ) {
-        return inventoryService.isInStock(productId);
+    public List<InventoryResponse> getProducts(@RequestParam(name = "productsId") List<Long> productsId) {
+        return inventoryService.getProductsFromInventory(productsId);
     }
 
 }
