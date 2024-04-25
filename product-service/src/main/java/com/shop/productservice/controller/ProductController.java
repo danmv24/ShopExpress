085c802cd2e691ace76136f4f2e69bbba7f10c31
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-25 18:42:13 +0300

    a new point has been written to get product IDs through their name

--- product-service/src/main/java/com/shop/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/shop/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.shop.productservice.service.ProductService;
 import com.shop.productservice.view.ProductView;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,12 @@ public class ProductController {
     @GetMapping
     public List<ProductView> getAllProducts() {
         return productService.getProducts();
+    }
+
+    @GetMapping("/getProductId")
+    @Transactional(readOnly = true)
+    public List<Long> getProductIdViaProductName(@RequestParam(name = "productNames") List<String> productNames) {
+        return productService.getProductIdViaProductName(productNames);
     }
 
 }
