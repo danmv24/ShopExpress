dbc433ea2a43c31e9cb2d2c5e23956df7f4cffad
danmv24 <{{N:dX"`zsFfoZ9>
2024-04-25 23:41:41 +0300

    information has been uploaded for testing

--- inventory-service/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,33 @@
 package com.shop.inventoryservice;
 
+import com.shop.inventoryservice.entity.InventoryEntity;
+import com.shop.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			InventoryEntity inventory = new InventoryEntity();
+			inventory.setProductId(2L);
+			inventory.setQuantity(3);
+
+			InventoryEntity inventory2 = new InventoryEntity();
+			inventory.setProductId(3L);
+			inventory.setQuantity(5);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+		};
 	}
 
 }
