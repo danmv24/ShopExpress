f53ca78ce67847d7bce2a98777e8099f5768f465
danmv24 <{{N:dX"`zsFfoZ9>
2024-05-13 12:12:12 +0300

    addProductInInventory

--- inventory-service/src/main/java/com/shop/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/controller/InventoryController.java
@@ -1,5 +1,6 @@
 package com.shop.inventoryservice.controller;
 
+import com.shop.inventoryservice.form.InventoryForm;
 import com.shop.inventoryservice.response.InventoryResponse;
 import com.shop.inventoryservice.service.InventoryService;
 import lombok.AllArgsConstructor;
@@ -19,6 +20,12 @@ public class InventoryController {
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> getProducts(@RequestParam(name = "productsId") List<Long> productsId) {
         return inventoryService.getProductsFromInventory(productsId);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProductInInventory(InventoryForm inventoryForm) {
+        inventoryService.addProduct(inventoryForm);
     }
 
 }
