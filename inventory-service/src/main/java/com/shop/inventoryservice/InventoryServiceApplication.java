ed5ab4efa96d196c7ed025a45aadf0e5c07785c5
danmv24 <{{N:dX"`zsFfoZ9>
2024-05-13 12:09:45 +0300

    added annotation @EnableFeignClients

--- inventory-service/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.shop.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
