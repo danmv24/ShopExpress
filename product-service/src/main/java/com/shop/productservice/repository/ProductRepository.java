80a584641c3d7f7bc70164ad004923aa903b612f
danmv24 <{{N:dX"`zsFfoZ9>
2024-05-13 12:08:40 +0300

    find by product name

--- product-service/src/main/java/com/shop/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/shop/productservice/repository/ProductRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
 
     List<ProductEntity> findAll();
 
-    List<ProductEntity> findByProductNameIn(List<String> productName);
+    List<ProductEntity> findByProductNameIn(List<String> productNames);
+
+    Optional<ProductEntity> findByProductName(String productName);
 
 }
