e57bc5ec818639045992edefa896ebe1bbc78c3c
danmv24 <{{N:dX"`zsFfoZ9>
2024-07-10 12:02:58 +0300

    the return value type has been changed from String to CompletableFuture<String>. Added annotations by @CircuitBreaker, @Timelimiter and @Retry. The fallbackmethod method is also written

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -2,11 +2,16 @@ package com.orderservice.controller;
 
 import com.orderservice.form.OrderForm;
 import com.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @AllArgsConstructor
@@ -16,9 +21,15 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public String createOrder(@RequestBody OrderForm orderForm) {
-        orderService.createOrder(orderForm);
-        return "Заказ обрабатывается";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> createOrder(@RequestBody OrderForm orderForm) {
+        return CompletableFuture.supplyAsync(() -> orderService.createOrder(orderForm));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderForm orderForm, RuntimeException runtimeException) {
+         return CompletableFuture.supplyAsync(() -> "Упс! Что-то пошло не так. Пожалуйста, сделайте заказ чуточку позже!" );
     }
 
 }
